b9cfa96320ce7b3269399ce6e870156a82b5f35b
eunyoungko <eunyoungko0529@gmail.com>
2022-12-27 15:07:24 +0900

    2212271507

---
+++ Abc/abc.java
@@ -0,0 +1,15 @@
+package Abc;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class abc extends JFrame {
+  JButton btn1 = new JButton("친구목록");
+  JButton btn2 = new JButton("맛집검색");
+  JButton btn3 = new JButton("친구목록");
+  JButton btn4 = new JButton("친구목록");
+
+  public static void main(String[] args) {
+    System.out.println("꺄하하하하하항");
+  }
+}
